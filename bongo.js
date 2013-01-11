7e21a2a99299c9f54b4233c85e3cd2e45f5e253a
Aaron Shafovaloff <aaronshaf@gmail.com>
2013-01-10 20:46:10 -0700

    No more Q; basic development

--- bongo.js
+++ bongo.js
@@ -1,47 +1,106 @@
-var bongo = (function(bongo,window,console,Q){
-  "use strict";
+(function(window){
+  //"use strict";
+  var bongo = {};
 
   window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
   window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
   window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
   if(!window.indexedDB) {
-    console.log('IndexDB not supported');
-    return bongo;
+    throw 'IndexedDB not supported';
   }
+
+  bongo.Collection = {};
+  Object.defineProperties(bongo.Collection, {
+    "objectStore": {
+      enumerable: false,
+      get: function(callback) {
+        var request = window.indexedDB.open(database.name, database.version);
+
+        request.onerror = function(event) {
+          throw request.webkitErrorMessage || request.error.name;
+        };
+
+        request.onsuccess = function(event) {
+          console.log('success');
+        };
+
+        request.onupgradeneeded = function(event) {
+          console.log('onupgradeneeded');
+
+          var db = event.target.result;
+          console.log(db);
+
+          database.collections.forEach(function(collection) {
+            console.log(collection);
+
+            if(db.objectStoreNames.contains(collection.name)) {
+              db.deleteObjectStore(collection.name);
+            }
+            var objectStore = db.createObjectStore(collection.name, {keyPath: "id",autoIncrement: true});
+
+            // objectStore.createIndex(collection.name + '__id', '_id', {unique: true});
+            // if(collection.indexes) {
+            //   collection.indexes.forEach(function(index) {
+
+            //   });
+            // }
+          });
+        };
+      }
+    },
+
+    "find": {
+      enumerable: false,
+      writable: false,
+      value: function() {
+        if(!this.collectionName) {
+          throw "Could not find collection name";
+        }
+
+        this.objectStore(function() {
+          
+        });
+
+        //.add(customerData[i]);
+      }
+    }
+  });
 
   bongo.defineDatabase = function(database) {
     if(!database.name || !database.version) {
-      console.log('Database name or version missing.');
+      window.console.log('Database name or version missing.');
       return false;
     }
 
+    var collections = [];
+
+    database.collections.forEach(function(collection) {
+      Object.defineProperty(database,collection.name,{
+
+      });
+    });
+
+    if(database.version instanceof Date) {
+      database.version = Math.round(database.version.getTime());
+    }
+
     Object.defineProperty(bongo,database.name, {
-      enumerable: false,
+      enumerable: true,
+      configurable: true,
       get: function() {
-        var defer = Q.defer();
-        var request = window.indexedDB.open(database.name, database.version);
-        request.onerror = function(event) {
-          defer.reject(request.errorCode);
-        };
-        request.onsuccess = function(event) {
-          defer.resolve(request.result);
-        };
-        request.onupgradeneeded = function(event) {
-          var db = event.target.result;
 
-          database.collections.forEach(function(collection) {
-            var objectStore = db.createObjectStore(collection.name, {keyPath: "_id"});
-            objectStore.createIndex(collection.name + '__id', '_id', {unique: true});
-            if(collection.indexes) {
-              collection.indexes.forEach(function(index) {
+        /*
 
-              });
-            }
-          });
-        };
-        return defer.promise;
+        */
       }
     });
+
+    return true;
   };
 
-}(bongo || {},window,console,Q));
+  window.bongo = bongo;
+
+  if ( typeof define === "function" && define.amd && define.amd.bongo ) {
+    define( "bongo", [], function () { return bongo; } );
+  }
+}(window));
