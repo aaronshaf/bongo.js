04841ff1c01d17c62e5e33c473cbb5f34c89ea27
Aaron Shafovaloff <aaronshaf@gmail.com>
2013-05-08 10:16:52 -0600

    Convenience method that spits out all db info

--- src/bongo.ts
+++ src/bongo.ts
@@ -22,4 +22,40 @@ module bongo {
 
     return bongo[definition.name];
   }
+  export var debug = false;
+
+  export function info(name = null) {
+    console.group('Bongo')
+    var request;
+
+    var debugDb = function(name) {
+      var request = window.indexedDB.open(name);
+      request.onsuccess = function() {
+        var db = event.target.result;
+        console.log(db);
+        var objectStoreNames = [];
+        for(var x = 0;x < db.objectStoreNames.length;x++) {
+          objectStoreNames.push(db.objectStoreNames.item(x));
+        }
+        console.log({
+          name: db.name,
+          objectStores: objectStoreNames,
+          version: db.version
+        });
+      };
+    }
+
+    if(name) {
+      debugDb(name);
+    } else {
+      request = window.indexedDB.webkitGetDatabaseNames();
+      request.onsuccess = function(event) {
+        var dbNameList = event.target.result;
+        for(var x = 0;x < dbNameList.length;x++) {
+          debugDb(dbNameList.item(x));
+        }
+      }
+    }
+    console.groupEnd();
+  }
 }
