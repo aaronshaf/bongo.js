7ca0315bf898cd870f2b7dd536b6010e3e5579f3
Aaron Shafovaloff <aaronshaf@gmail.com>
2013-05-18 07:32:51 -0600

    Stop testing with shim for now.

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -3,7 +3,7 @@ urlRoot = '/karma/';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  '../libs/indexedDBShim.js',
+  // '../libs/indexedDBShim.js',
   '../src/bongo.es5.js',
   'spec/*Spec.js'
 ];
@@ -14,6 +14,6 @@ runnerPort = 9100;
 colors = true;
 logLevel = LOG_INFO;
 autoWatch = true;
-browsers = ['Chrome','Safari','Opera','ChromeCanary','Firefox']; //,'IE' ,
+browsers = ['Chrome','ChromeCanary','Firefox']; //,'IE' ,
 captureTimeout = 60000;
 singleRun = false;
