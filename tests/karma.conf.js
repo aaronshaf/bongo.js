bcfcc8befbc4f6a5c88681936f0fa2d43f8ebaf0
Aaron Shafovaloff <aaronshaf@gmail.com>
2013-05-20 19:39:18 -0600

    removed autoWatch line

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -13,7 +13,6 @@ port = 9876;
 runnerPort = 9100;
 colors = true;
 logLevel = LOG_INFO;
-autoWatch = true;
 browsers = ['Chrome','ChromeCanary','Firefox']; // Add 'IE' if you're on Windows
 captureTimeout = 60000;
 singleRun = true;
